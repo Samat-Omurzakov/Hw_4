633c5d364e4c728ea5d4f24a550b9e9d25863574
Samat-Omurzakov <samat-omurzakov@mail.ru>
2023-03-01 20:37:23 +0600

    z

--- KeyBoard/Client_kb.py
+++ KeyBoard/Client_kb.py
@@ -14,3 +14,30 @@ location = KeyboardButton('location', request_location=True)
 contact = KeyboardButton('contact', request_contact=True)
 
 start_markup.add(start_button, info_button, quiz_button, location, contact, reg_button)
+
+cancel = KeyboardButton('Cancel')
+
+submit_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    KeyboardButton('Пушка'),
+    KeyboardButton('Так себе'),
+    cancel
+)
+
+gender_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    KeyboardButton('Мужчина'),
+    KeyboardButton('Женщина'),
+    cancel
+)
+
+cancel_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    cancel
+)
--- handler/fsm_anketa.py
+++ handler/fsm_anketa.py
@@ -2,6 +2,7 @@ from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from KeyBoard.Client_kb import *
 
 
 class FSMAdmin(StatesGroup):
@@ -10,14 +11,24 @@ class FSMAdmin(StatesGroup):
     photo = State()
     gender = State()
     region = State()
+    submit = State()
 
 
 async def fsm_start(message: types.Message):
     if message.chat.type == 'private':
         await FSMAdmin.name.set()
-        await message.answer('Кто ты воин?')
+        await message.answer('Кто ты воин?', reply_markup=cancel_markup)
     else:
         await message.answer('Только 1vs1')
+
+
+async def load_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as date:
+        date['id'] = message.from_user.id
+        date['username'] = message.from_user.username
+        date['name'] = message.text
+    await FSMAdmin.next()
+    await message.answer('Сколько годиков?', reply_markup=cancel_markup)
 
 
 async def fsm_age(message: types.Message, state: FSMContext):
@@ -26,7 +37,7 @@ async def fsm_age(message: types.Message, state: FSMContext):
             async with state.proxy() as date:
                 date['age'] = int(message.text)
                 await FSMAdmin.gender.set()
-                await message.answer('Пол?')
+                await message.answer('Пол?', reply_markup=gender_markup)
         elif int(message.text) < 18:
             await message.answer('Маловат школьник')
         elif 99 < int(message.text):
@@ -40,7 +51,7 @@ async def fsm_gender(message: types.Message, state: FSMContext):
         async with state.proxy() as date:
             date['gender'] = message.text
             await FSMAdmin.next()
-            await message.answer('Скажи где ты находишся?')
+            await message.answer('Скажи где ты находишся?', reply_markup=cancel_markup)
     else:
         await message.answer('Быть не может')
 
@@ -49,23 +60,45 @@ async def fsm_region(message: types.Message, state: FSMContext):
     if message.text.isalpha():
         async with state.proxy() as date:
             date['region'] = message.text
-            await message.answer('DONE')
+            await message.answer('Фотку давай', reply_markup=cancel_markup)
+            await FSMAdmin.photo.set()
     else:
         await message.answer('Не нашел')
 
 
-async def load_name(message: types.Message, state: FSMContext):
+async def fsm_photo(message: types.Message, state: FSMContext):
     async with state.proxy() as date:
-        date['id'] = message.from_user.id
-        date['username'] = message.from_user.username
-        date['name'] = message.text
-    await FSMAdmin.next()
-    await message.answer('Сколько годиков?')
+        date['photo'] = message.photo[0].file_id
+        await message.answer_photo(date['photo'],
+                                   caption=f'{date["name"]} {date["age"]} {date["gender"]} @{date["username"]}')
+    await FSMAdmin.submit.set()
+    await message.answer('Ну как?', reply_markup=submit_markup)
+
+
+async def submit(message: types.Message, state: FSMContext):
+    if message.text == 'Пушка':
+        await message.answer('DONE!')
+        await state.finish()
+    elif message.text == 'Так себе':
+        await message.answer('Кто ты воин?')
+        await FSMAdmin.name.set()
+    else:
+        await message.answer('???')
+
+
+async def cancel_reg(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is not None:
+        await state.finish()
+        await message.answer('Пока')
 
 
 def reg_hand_anketa(db: Dispatcher):
+    db.register_message_handler(cancel_reg, Text(equals='cancel', ignore_case=True), state='*')
     db.register_message_handler(fsm_start, commands=['reg'])
     db.register_message_handler(load_name, state=FSMAdmin.name)
     db.register_message_handler(fsm_age, state=FSMAdmin.age)
     db.register_message_handler(fsm_gender, state=FSMAdmin.gender)
     db.register_message_handler(fsm_region, state=FSMAdmin.region)
+    db.register_message_handler(fsm_photo, state=FSMAdmin.photo, content_types=['photo'])
+    db.register_message_handler(submit, state=FSMAdmin.submit)
